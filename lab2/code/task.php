ce9495917115aaa34ee38d6bcbbecab7ea24eeb8
enui <115658651+enuiii@users.noreply.github.com>
2024-03-17 23:38:33 +0200

    Update task.php

--- lab2/code/task.php
+++ lab2/code/task.php
@@ -185,3 +185,56 @@ function sumDigits($num) {
 }
 
 echo sumDigits(12345) . "\n";
+
+echo ("Task 17\n");
+$array = [];
+for ($i = 1; $i <= 5; $i++) {
+	$array[] = str_repeat('x', $i);
+}
+print_r($array);
+
+function arrayFill($value, $count) {
+	$result = [];
+	for ($i = 0; $i < $count; $i++) {
+		$result[] = $value;
+	}
+	return $result;
+}
+print_r(arrayFill('x', 5));
+
+$numbers = [[1, 2, 3], [4, 5], [6]];
+$sum = 0;
+foreach ($numbers as $subArray) {
+	$sum += array_sum($subArray);
+}
+echo $sum;
+
+$array = [];
+$count = 1;
+for ($i = 0; $i < 3; $i++) {
+	$subArray = [];
+	for ($j = 0; $j < 3; $j++) {
+		$subArray[] = $count++;
+	}
+	$array[] = $subArray;
+}
+print_r($array);
+
+$array = [2, 5, 3, 9];
+$result = ($array[0] * $array[1]) + ($array[2] * $array[3]);
+echo $result."\n";;
+
+$user = ['name' => 'Vladislav', 'surname' => 'Karpichev', 'patronymic' => 'Andreevich'];
+echo $user['surname'] . ' ' . $user['name'] . ' ' . $user['patronymic']."\n";;
+
+$date = ['year' => date('Y'), 'month' => date('m'), 'day' => date('d')];
+echo $date['year'] . '-' . $date['month'] . '-' . $date['day']."\n";;
+
+$arr = ['a','b','c','d','e'];
+echo count($arr)."\n";;
+
+$arr = ['a','b','c','d','e'];
+echo end($arr)."\n";;
+
+$lastIndex = count($arr) - 2;
+echo $arr[$lastIndex]."\n";
