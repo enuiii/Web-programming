05238a0fbb71d115b0082907eaacfd25cc66a277
enui <115658651+enuiii@users.noreply.github.com>
2024-03-17 23:35:24 +0200

    Create task.php

---
+++ lab2/code/task.php
@@ -0,0 +1,12 @@
+<?php
+echo ("Task 1\n");
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+
+$order =& $very_bad_unclear_name;
+$order .= ' and Coca Cola, please';
+
+// Don't change the line below
+echo "Your order is: $very_bad_unclear_name.\n";
