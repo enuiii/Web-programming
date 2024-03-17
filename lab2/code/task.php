e83a7ba2559e2711397794c672d01b280709e00f
enui <115658651+enuiii@users.noreply.github.com>
2024-03-17 23:35:46 +0200

    Update task.php

--- lab2/code/task.php
+++ lab2/code/task.php
@@ -10,3 +10,16 @@ $order .= ' and Coca Cola, please';
 
 // Don't change the line below
 echo "Your order is: $very_bad_unclear_name.\n";
+
+echo ("Task 2\n");
+$number1 = 10;
+echo $number1;
+echo "\n";
+$number2 = 5.5;
+echo $number2;
+echo "\n";
+echo $number1 + $number2;
+$last_month = 1187.23;
+$this_month = 1089.98;
+$difference = $last_month - $this_month;
+echo "\nРазница между расходами в прошлом месяце и в этом месяце: " . $difference . "\n";
