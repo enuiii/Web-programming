bee4f44d1d16d1043e36bec266f5e491a7a1bb91
enui <115658651+enuiii@users.noreply.github.com>
2024-05-04 12:13:01 +0200

    task1 adding

---
+++ lab3/code/task1_a_b/task1.php
@@ -0,0 +1,11 @@
+<?php
+echo 'Task A:'."<br>";
+$str = 'ahb acb aeb aeeb adcb axeb';
+preg_match_all('/a..b/', $str, $matches);
+print_r($matches[0]);
+echo "<br>";
+
+echo 'Task B:'."<br>";
+$str = 'a1b2c3';
+$result = preg_replace_callback('/\d/', function($match) {return $match[0] ** 3;}, $str);
+echo $result;
